f4ec6ae8c959fe528502c0e42a3ea860060d0561
damhoe <damian.hoedtke@gmx.de>
2023-11-29 22:53:36 +0100

    Upgrade gradle to 8.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,10 @@ android {
 }
 
 dependencies {
-    val navVersion = "2.7.4"
+    val navVersion = "2.7.5"
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     implementation("androidx.transition:transition-ktx:1.4.1")
